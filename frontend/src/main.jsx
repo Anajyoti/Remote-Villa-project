76d13722811d0a0482d7255ebfecc99accc7a3f2
Anajyoti <anajyoti99@gmail.com>
2024-10-11 02:11:38 -0400

    phase-0

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,23 @@
 // frontend/src/main.jsx
-import React from 'react';
+import  React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
 import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 //Create a variable to access your store and expose it on the window
 const store = configureStore();
+if (import.meta.env.MODE !== 'production') {
+  restoreCSRF();
 
-if (process.env.NODE_ENV !== 'production') {
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
+
+// if (process.env.NODE_ENV !== 'production') {
+//   window.store = store;
+// }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
