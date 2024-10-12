abde9cf6749f65916af8b9020dc28b7e71674f8c
Anajyoti <anajyoti99@gmail.com>
2024-10-11 23:16:00 -0400

    phase-5

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,15 @@
-// frontend/src/main.jsx
-import  React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
-import * as sessionActions from './store/session'; 
+import * as sessionActions from './store/session';
 import { Modal, ModalProvider } from './context/Modal';
-//Create a variable to access your store and expose it on the window
+
 const store = configureStore();
+
 if (import.meta.env.MODE !== 'production') {
   restoreCSRF();
 
@@ -18,16 +18,14 @@ if (import.meta.env.MODE !== 'production') {
   window.sessionActions = sessionActions;
 }
 
-// if (process.env.NODE_ENV !== 'production') {
-//   window.store = store;
-// }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <ModalProvider>
       <Provider store={store}>
         <App />
+        <Modal />
       </Provider>
     </ModalProvider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
