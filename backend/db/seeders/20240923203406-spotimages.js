1c71548879f969401d83e076e09e2d0addd2795d
Anajyoti <anajyoti99@gmail.com>
2024-10-20 01:52:00 -0400

    change in seeders

--- backend/db/seeders/20240923203406-spotimages.js
+++ backend/db/seeders/20240923203406-spotimages.js
@@ -14,144 +14,164 @@ module.exports = {
     await SpotImages.bulkCreate([
       {
         spotId: 1,
-        url: "/images/01.png",
-        preview: false,
+        url: "/preview1.jpeg",
+        preview: true,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         spotId: 2,
-        url: "/images/02.png",
-        preview: false,
+        url: "/preview2.jpeg",
+        preview: true,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         spotId: 3,
-        url: "/images/03.png",
-        preview: false,
+        url: "/preview3.jpeg",
+        preview: true,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         spotId: 4,
-        url: "/images/04.png",
+        url: "/preview4.jpeg",
         preview: true,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         spotId: 5,
-        url: "/images/05.png",
+        url: "/preview5.jpeg",
         preview: true,
         createdAt: new Date(),
         updatedAt: new Date()
-      },
+       },
       {
         spotId: 6,
-        url: "/images/06.png",
-        preview: false,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        spotId: 7,
-        url: "/images/07.png",
-        preview: false,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        spotId: 8,
-        url: "/images/08.png",
+        url: "/preview6.jpeg",
         preview: true,
         createdAt: new Date(),
         updatedAt: new Date()
       },
-      {
-        spotId: 9,
-        url: "/images/09.png",
-        preview: false,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        spotId: 10,
-        url: "/images/10.png",
-        preview: true,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        spotId: 11,
-        url: "/images/011.png",
-        preview: false,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        spotId: 12,
-        url: "/images/012.png",
-        preview: false,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        spotId: 13,
-        url: "/images/013.png",
-        preview: false,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        spotId: 14,
-        url: "/images/014.png",
-        preview: false,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        spotId: 15,
-        url: "/images/015.png",
-        preview: false,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        spotId: 16,
-        url: "/images/016.png",
-        preview: true,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        spotId: 17,
-        url: "/images/017.png",
-        preview: false,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        spotId: 18,
-        url: "/images/018.png",
-        preview: true,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        spotId: 19,
-        url: "/images/019.png",
-        preview: false,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        spotId: 20,
-        url: "/images/20.png",
-        preview: true,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }
+      // {
+      //   spotId: 7,
+      //   url: "/images/07.png",
+      //   preview: false,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // },
+      // {
+      //   spotId: 8,
+      //   url: "/images/08.png",
+      //   preview: true,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // },
+      // {
+      //   spotId: 9,
+      //   url: "/images/09.png",
+      //   preview: false,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // },
+      // {
+      //   spotId: 10,
+      //   url: "/images/10.png",
+      //   preview: true,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // },
+      // {
+      //   spotId: 11,
+      //   url: "/images/011.png",
+      //   preview: false,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // },
+      // {
+      //   spotId: 12,
+      //   url: "/images/012.png",
+      //   preview: false,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // },
+      // {
+      //   spotId: 13,
+      //   url: "/images/013.png",
+      //   preview: false,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // },
+      // {
+      //   spotId: 14,
+      //   url: "/images/014.png",
+      //   preview: false,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // },
+      // {
+      //   spotId: 15,
+      //   url: "/images/015.png",
+      //   preview: false,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // },
+      // {
+      //   spotId: 16,
+      //   url: "/images/016.png",
+      //   preview: true,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // },
+      // {
+      //   spotId: 17,
+      //   url: "/images/017.png",
+      //   preview: false,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // },
+      // {
+      //   spotId: 18,
+      //   url: "/images/018.png",
+      //   preview: true,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // },
+      // {
+      //   spotId: 19,
+      //   url: "/images/019.png",
+      //   preview: false,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // },
+      // {
+      //   spotId: 20,
+      //   url: "/images/20.png",
+      //   preview: true,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // }
+      {spotId:1, url: '/detail1.jpeg', preview: false},
+      {spotId:1, url: '/detail2.jpeg', preview: false},
+      {spotId:1, url: '/detail3.jpeg', preview: false},
+      {spotId:1, url: '/detail4.jpeg', preview: false},
+
+      {spotId:2, url: '/detail1.jpeg', preview: false},
+      {spotId:2, url: '/detail2.jpeg', preview: false},
+      {spotId:2, url: '/detail3.jpeg', preview: false},
+      {spotId:2, url: '/detail4.jpeg', preview: false},
+
+      {spotId:3, url: '/detail1.jpeg', preview: false},
+      {spotId:3, url: '/detail2.jpeg', preview: false},
+      {spotId:3, url: '/detail3.jpeg', preview: false},
+      {spotId:3, url: '/detail4.jpeg', preview: false},
+
+      {spotId:4, url: '/detail1.jpeg', preview: false},
+      {spotId:4, url: '/detail2.jpeg', preview: false},
+      {spotId:4, url: '/detail3.jpeg', preview: false},
+      {spotId:4, url: '/detail4.jpeg', preview: false},
+
     ],{validate: true});
   },
 
--- backend/db/seeders/20240923233505-reviews.js
+++ backend/db/seeders/20240923233505-reviews.js
@@ -17,19 +17,19 @@ module.exports = {
 
    await reviews.bulkCreate([
     {
-      userId: 5,
+      userId: 3,
       spotId: 1,
       "review": "Great hideaway, super place to decompress and take in the nature.",
       "stars": 5.0
     },
     {
-      userId: 6,
+      userId: 2,
       spotId: 1,
       "review": "we will absolutely come back next season!",
       "stars": 5.0
     },
     {
-      userId: 3,
+      userId: 1,
       spotId: 2,
       "review": "This is super cool and awsome!",
       "stars": 5.0
@@ -40,67 +40,67 @@ module.exports = {
       "review": "beautiful house and amazing view!",
       "stars": 5.0
     },
-    {
-      userId: 4,
-      spotId: 4,
-      "review": "comforting and cozy environment",
-      "stars": 5.0
-    },
-    {
-      userId: 5,
-      spotId: 5,
-      "review": "Fantastic house, and dock, views and host!",
-      "stars": 5.0
-    },
+    // {
+    //   userId: 4,
+    //   spotId: 4,
+    //   "review": "comforting and cozy environment",
+    //   "stars": 5.0
+    // },
+    // {
+    //   userId: 5,
+    //   spotId: 5,
+    //   "review": "Fantastic house, and dock, views and host!",
+    //   "stars": 5.0
+    // },
 
-    {
-      userId: 2,
-      spotId: 3,
-      "review": "Great place to rent",
-      "stars": 5.0
-    },
-    {
-      userId: 6,
-      spotId: 1,
-      "review": "This is such a fantastic experience ever! ",
-      "stars": 5.0
-    },
-    {
-      userId: 12,
-      spotId: 5,
-      "review": "This is drop-dead awsome! cool activity to do around here.",
-      "stars": 5.0
-    },
-    {
-      userId: 2,
-      spotId: 4,
-      "review": "I can't say enough good things about this place. The hosts were communicative and clear, and made our stay easy and hassle-free. The house is massive.",
-      "stars": 5.0
-    },
-    {
-      userId: 4,
-      spotId: 7,
-      "review": "Beautiful place to stay. It snowed (which we weren't expecting) and it turned out to be amazing, tons of places to cozy up under a blanket and enjoy the company of friends and family. Highly recommend ",
-      "stars": 5.0
-    },
-    {
-      userId: 20,
-      spotId: 6,
-      "review": "Beautiful and large space, the creek running through the backyard was especially nice, hosts were very communicative.",
-      "stars": 5.0
-    },
-    {
-      userId: 3,
-      spotId: 11,
-      "review": "The house was unique and the river/surrounding area very pretty. There was plenty to do inside and out. Communication was excellent and lightning fast. ",
-      "stars": 5.0
-    },
-    {
-      userId: 20,
-      spotId: 2,
-      "review": "Great location and a fun time with all the amenities on the property!",
-      "stars": 5.0
-    },
+    // {
+    //   userId: 2,
+    //   spotId: 3,
+    //   "review": "Great place to rent",
+    //   "stars": 5.0
+    // },
+    // {
+    //   userId: 3,
+    //   spotId: 1,
+    //   "review": "This is such a fantastic experience ever! ",
+    //   "stars": 5.0
+    // },
+    // {
+    //   userId: 2,
+    //   spotId: 5,
+    //   "review": "This is drop-dead awsome! cool activity to do around here.",
+    //   "stars": 5.0
+    // },
+    // {
+    //   userId: 2,
+    //   spotId: 4,
+    //   "review": "I can't say enough good things about this place. The hosts were communicative and clear, and made our stay easy and hassle-free. The house is massive.",
+    //   "stars": 5.0
+    // },
+    // {
+    //   userId: 2,
+    //   spotId: 7,
+    //   "review": "Beautiful place to stay. It snowed (which we weren't expecting) and it turned out to be amazing, tons of places to cozy up under a blanket and enjoy the company of friends and family. Highly recommend ",
+    //   "stars": 5.0
+    // },
+    // {
+    //   userId: 1,
+    //   spotId: 6,
+    //   "review": "Beautiful and large space, the creek running through the backyard was especially nice, hosts were very communicative.",
+    //   "stars": 5.0
+    // },
+    // {
+    //   userId: 3,
+    //   spotId: 11,
+    //   "review": "The house was unique and the river/surrounding area very pretty. There was plenty to do inside and out. Communication was excellent and lightning fast. ",
+    //   "stars": 5.0
+    // },
+    // {
+    //   userId: 1,
+    //   spotId: 2,
+    //   "review": "Great location and a fun time with all the amenities on the property!",
+    //   "stars": 5.0
+    // },
    ],{validate: true});
   },
 
